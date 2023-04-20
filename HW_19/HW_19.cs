0d74b5687f4edb34de779fa16c10be981fbd264f
Olga Dobrydina <doa1981@mail.ru>
2023-04-20 21:25:32 +0500

    Palindrom

---
+++ HW_19/HW_19.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход пятизначное
+//число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+Console.Write("Введите пятизначное число ");
+int number = int.Parse(Console.ReadLine()); 
+int num1 = number / 10000 % 10;
+int num2 = number / 1000 % 10;
+int rev1 = number / 10 % 10;
+int rev2 = number % 10;
+int res = number / 10000;
+if (res < 1 || res > 9)
+{
+    Console.WriteLine(number + " -> " + "Это число не пятизначное");
+}
+else if  (num1 == rev2 && num2 == rev1)
+{
+    Console.WriteLine(number + " -> Палиндром");
+}
+else
+{
+    Console.WriteLine(number + " -> Не палиндром");
+}
---
+++ HW_23/HW_23.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число ");
+string input = Console.ReadLine();
+int Number = int.Parse(input);
+if (Number > 1)
+{
+   for (int i = 1; i <= Number; i++)
+    {
+        Console.WriteLine($"{i}*{i}*{i} = {i*i*i}");
+    }
+}
+else
+{
+    Console.WriteLine("Данные для таблицы отсутствуют ");
+}
