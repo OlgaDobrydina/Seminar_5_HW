90beca4981f281d7dcb5560ea78cebd7f3a5f0f7
Olga Dobrydina <doa1981@mail.ru>
2023-04-23 22:43:58 +0500

    AllSumma

---
+++ HW_27/HW_27.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int PrintNumber(string mes)
+{
+    Console.WriteLine(mes);
+    string input = Console.ReadLine();
+    int number = int.Parse(input);
+    return number;
+}
+
+int AllSumma(int number)
+{
+    int sum = 0;
+    while(number > 0)
+    {    
+        sum += number % 10;
+        number = number / 10;
+    }
+    return sum;
+}
+
+int number = PrintNumber("Введите число ");
+Console.WriteLine(AllSumma(number));
